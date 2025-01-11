489c284941fff70ba89bb818d467e47731bee762
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-11 21:51:26 +0530

    Day 16 progress

--- WeSplit-HackingwithSwift-App/ContentView.swift
+++ WeSplit-HackingwithSwift-App/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var nameTextfieldText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Enter your name", text: $nameTextfieldText)
+            Text("You name is \(nameTextfieldText)!")
         }
-        .padding()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        Button("Tap Count: \(tapCount)") {
+//            tapCount += 1
+//        }
+        
+        
+        
+        
+//        NavigationStack {
+//            Form {
+//                Section {
+//                    Text("This new text")
+//                    Text("This new text")
+//                } header: {
+//                    Text("First Section")
+//                }
+//                
+//                Section {
+//                    Text("This new text")
+//                    Text("This new text")
+//                    Text("This new text")
+//                } header: {
+//                    Text("Second Section")
+//                }
+//            }
+//            .navigationTitle("Bill Split App")
+//            .navigationBarTitleDisplayMode(.automatic)
+//        }
     }
 }
 

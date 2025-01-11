06b308d50a08ce5c02855bbf56f911dc20d38d23
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-11 21:24:39 +0530

    Initial Commit

---
+++ WeSplit-HackingwithSwift-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit-HackingwithSwift-App
+//
+//  Created by Shubham on 1/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit-HackingwithSwift-App/WeSplit_HackingwithSwift_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_HackingwithSwift_AppApp.swift
+//  WeSplit-HackingwithSwift-App
+//
+//  Created by Shubham on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_HackingwithSwift_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

adb4c8c87c9a429646df40ee119b266874f3dff4
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-11 23:00:25 +0530

    Done Project

--- WeSplit-HackingwithSwift-App/ContentView.swift
+++ WeSplit-HackingwithSwift-App/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - Properties
+    @FocusState private var amountIsFocused: Bool
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     let tipPercentagesArray = [10, 15, 20, 25, 0]
     let currencyLocationID = Locale.current.currency?.identifier ?? "USD"
+    
+    var grandTotalAmount: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+
+        return grandTotal
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
@@ -33,6 +43,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: currencyLocationID))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<51) {
@@ -52,8 +63,24 @@ struct ContentView: View {
                     Text("How much tip do you want to leave?")
                 }
                 
+                Section("grand total after tip") {
+                    Text(grandTotalAmount, format: .currency(code: currencyLocationID))
+                }
+
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: currencyLocationID))
+                }
+                
             }
             .navigationTitle("Bill split app")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
